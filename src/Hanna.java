1c67f903bfe4695d7f98c898fe2e1798215e7759
netta <nettastor@gmail.com>
2022-04-23 00:58:38 -0400

    Added Hanna file and third print statement.

--- src/Hanna.java
+++ src/Hanna.java
@@ -2,5 +2,6 @@ public class Hanna {
     public static void main(String[] args) {
         System.out.println("Conflict");
         System.out.println("Fix");
+        System.out.println("Easy");
     }
 }
