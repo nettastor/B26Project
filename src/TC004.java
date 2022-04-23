2b0f275bfdcff891cc72832464136410ab1cb177
netta <nettastor@gmail.com>
2022-04-23 00:24:45 -0400

    Added third print statement.

--- src/TC004.java
+++ src/TC004.java
@@ -2,5 +2,6 @@ public class TC004 {
     public static void main(String[] args) {
         System.out.println("Today");
         System.out.println("Tomorrow");
+        System.out.println("Yesterday");
     }
 }
